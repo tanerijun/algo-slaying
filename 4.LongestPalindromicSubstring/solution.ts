56da3885bc5a4f6414c925bc4c2fc1d7ab60c8ca
tanerijun <tanerivince@gmail.com>
2022-10-22 12:28:57 +0800

    4.LongestPalindromicSubstring

---
+++ 4.LongestPalindromicSubstring/solution.ts
@@ -0,0 +1,29 @@
+function expandAroundCenter(s: string, l: number, r: number): number {
+
+	while (l >= 0 && r < s.length && s[l] === s[r]) {
+		l--;
+		r++;
+	}
+
+	return r - l - 1;
+}
+
+export function longestPalindrome(s: string): string {
+	if (s.length < 1) return "";
+
+	// Keep track of the answer
+	let lIdx = 0;
+	let rIdx = 0;
+
+	for (let i = 0; i < s.length; i++) {
+		let caseOdd = expandAroundCenter(s, i, i); // ex: babad
+		let caseEven = expandAroundCenter(s, i, i + 1); // ex: abbd
+		const len = Math.max(caseOdd, caseEven);
+		if (len > rIdx - lIdx) {
+			lIdx = i - Math.floor((len - 1) / 2);
+			rIdx = i + Math.floor(len / 2);
+		}
+	}
+
+	return s.slice(lIdx, rIdx + 1);
+}
---
+++ 4.LongestPalindromicSubstring/test.ts
@@ -0,0 +1,12 @@
+import { assert, assertEquals } from "https://deno.land/std@0.160.0/testing/asserts.ts";
+import { longestPalindrome } from "./solution.ts";
+
+Deno.test("babad", () => {
+  assert(["bab", "aba"].includes(longestPalindrome("babad")));
+})
+
+Deno.test("cbbd", () => {
+  assertEquals(longestPalindrome("cbbd"), "bb");
+})
+
+longestPalindrome("cbba");
