7b4c5b84dd11f2f9018d592d90c24b2de24fa447
tanerijun <tanerivince@gmail.com>
2022-10-23 13:35:03 +0800

    6.Zigzag Conversion

---
+++ 6.ZigzagConversion/solution.ts
@@ -0,0 +1,26 @@
+export function convert(s: string, numRows: number): string {
+
+  const resArr: string[] = Array(numRows).fill("");
+  let cursor: number = 0;
+  let isDecreasing: boolean = false;
+
+  for (let i = 0; i < s.length; i++) {
+    resArr[cursor] += s[i];
+
+    if (cursor === 0) {
+      isDecreasing = false;
+    }
+
+    if (cursor === numRows - 1) {
+      isDecreasing = true;
+    }
+
+    if (isDecreasing) {
+      if (cursor != 0) cursor -= 1;
+    } else {
+      cursor += 1;
+    }
+  }
+
+  return resArr.join("");
+};
