247f1e7486c834d83a14f3beaab14a99c8fb844a
tanerijun <tanerivince@gmail.com>
2022-10-28 13:16:24 +0800

    11.ContainerWithMostWater

---
+++ 11.ContainerWithMostWater/solution.ts
@@ -0,0 +1,19 @@
+export function maxArea(height: number[]): number {
+  let max: number = 0;
+  let l: number = 0;
+  let r: number = height.length - 1;
+
+  while (l < r) {
+    const w = r - l;
+    const h = Math.min(height[l], height[r]);
+    max = Math.max(max, w * h);
+
+    if (height[l] < height[r]) {
+      l++;
+    } else {
+      r--;
+    }
+  }
+
+  return max;
+};
