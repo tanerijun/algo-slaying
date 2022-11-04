6c2660321c283354a8c7708fd415065a65723a68
tanerijun <tanerivince@gmail.com>
2022-11-04 23:32:45 +0800

    167.TwoSum2

---
+++ 167.TwoSum2/solution.ts
@@ -0,0 +1,22 @@
+export function twoSum(numbers: number[], target: number): number[] {
+  const res: number[] = [];
+
+  let l = 0;
+  let r = numbers.length - 1;
+
+  while (l < r) {
+    const sum = numbers[l] + numbers[r];
+
+    if (sum < target || (l > 0 && numbers[l] === numbers[l - 1])) {
+      l++;
+    } else if (sum > target || (r < numbers.length - 1 && numbers[r] === numbers[r + 1])) {
+      r--;
+    } else {
+      res.push(l + 1, r + 1);
+      l++;
+      r--;
+    }
+  }
+
+  return res;
+};
---
+++ 167.TwoSum2/test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "https://deno.land/std@0.160.0/testing/asserts.ts";
+import { twoSum } from "./solution.ts";
+
+Deno.test("Input: numbers = [2,7,11,15], target = 9", () => {
+  assertEquals(twoSum([2, 7, 11, 15], 9), [1, 2]);
+})
+
+Deno.test("Input: numbers = [2,3,4], target = 6", () => {
+  assertEquals(twoSum([2, 3, 4], 6), [1, 3]);
+})
+
+Deno.test("Input: numbers = [-1, 0], target = -1", () => {
+  assertEquals(twoSum([-1, 0], -1), [1, 2]);
+})
