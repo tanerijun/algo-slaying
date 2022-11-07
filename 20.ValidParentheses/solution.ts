6963738b841aa8cc0653e7c4fb71e511c3326778
tanerijun <tanerivince@gmail.com>
2022-11-07 21:36:24 +0800

    20.ValidParenthesis

---
+++ 20.ValidParentheses/solution.ts
@@ -0,0 +1,28 @@
+export function isValid(s: string): boolean {
+
+  if (!s || s.length % 2 != 0) {
+    return false;
+  }
+
+  const stack: string[] = [];
+
+  for (let i = 0; i < s.length; i += 1) {
+    switch (s[i]) {
+      case '{':
+        stack.push('}');
+        break;
+      case '[':
+        stack.push(']');
+        break;
+      case '(':
+        stack.push(')');
+        break;
+      default:
+        if (stack.pop() !== s[i]) {
+          return false;
+        }
+    }
+  }
+
+  return stack.length === 0;
+};
