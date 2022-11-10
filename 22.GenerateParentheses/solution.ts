5d573f166b5ccc7ca910cbe548e6d45c529eb59a
tanerijun <tanerivince@gmail.com>
2022-11-10 23:20:10 +0800

    22.GenerateParentheses

---
+++ 22.GenerateParentheses/solution.ts
@@ -0,0 +1,25 @@
+export function generateParenthesis(n: number): string[] {
+  const res: string[] = [];
+  backtrack(res, "", 0, 0, n);
+  return res;
+};
+
+function backtrack(ans: String[], cur: String, open: number, close: number, max: number) {
+  // max * 2 is max length of the string
+  // ex: if n = 3, then the possible string: "((()))", "(()())", "(())()", "()(())", "()()()"
+  // are all of length n * 2 = 6
+  if (cur.length === max * 2) {
+    ans.push(cur);
+    return;
+  }
+
+  // The max amounts of "(" is n
+  if (open < max) {
+    backtrack(ans, cur + "(", open + 1, close, max);
+  }
+
+  // There shouldn't be more ")" than "("
+  if (close < open) {
+    backtrack(ans, cur + ")", open, close + 1, max);
+  }
+}
---
+++ 22.GenerateParentheses/test.ts
@@ -0,0 +1,10 @@
+import { assertArrayIncludes, assertEquals } from "https://deno.land/std@0.160.0/testing/asserts.ts";
+import { generateParenthesis } from "./solution.ts";
+
+Deno.test("n = 3", () => {
+  assertArrayIncludes(generateParenthesis(3), ["((()))", "(()())", "(())()", "()(())", "()()()"])
+})
+
+Deno.test("n = 1", () => {
+  assertArrayIncludes(generateParenthesis(1), ["()"])
+})
