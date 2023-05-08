457286190004fccf362aaa58403151ea838b67d5
tanerijun <tanerivince@gmail.com>
2023-05-08 22:38:50 +0800

    Solve Leetcode 1672

---
+++ leetcode/001672.richest-customer-wealth.ts
@@ -0,0 +1,13 @@
+function maximumWealth(accounts: number[][]): number {
+	// Basically just find the highest value from the sum of each inner array
+	let max = 0
+
+	for (let i = 0; i < accounts.length; i++) {
+		max = Math.max(
+			max,
+			accounts[i].reduce((a, b) => a + b)
+		)
+	}
+
+	return max
+}
