075b65094c1c641070965bbb9f384e62337ca8e9
tanerijun <tanerivince@gmail.com>
2023-05-09 23:06:39 +0800

    Solve Leetcode 1342

---
+++ leetcode/001342.number-of-steps-to-reduce-a-number-to-zero.ts
@@ -0,0 +1,17 @@
+function numberOfSteps(num: number): number {
+	let steps = 0
+
+	while (num !== 0) {
+		if (num % 2 !== 0) {
+			num--
+		} else {
+			num /= 2
+		}
+
+		steps++
+	}
+
+	return steps
+}
+// Time complexity: O(log n) - The time complexity from executing `num--` is irrelevant relative to `num /= 2`
+// Space complexity: O(1)
