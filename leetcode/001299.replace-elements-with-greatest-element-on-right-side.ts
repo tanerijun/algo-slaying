152736023b4c6412118dcf9c49f4c0c08e96337a
tanerijun <tanerivince@gmail.com>
2023-05-13 13:14:55 +0800

    Solve Leetcode 1299

---
+++ leetcode/001299.replace-elements-with-greatest-element-on-right-side.ts
@@ -0,0 +1,14 @@
+function replaceElements(arr: number[]): number[] {
+	let currentMax = arr[arr.length - 1]
+	arr[arr.length - 1] = -1 // the last element is always -1
+
+	for (let i = arr.length - 2; i >= 0; i--) {
+		const temp = arr[i]
+		arr[i] = currentMax
+		currentMax = Math.max(currentMax, temp)
+	}
+
+	return arr
+}
+// Time complexity: O(n)
+// Space complexity: O(1)
