2d1e097b2a0fafe573db0a83838350f632590ef2
tanerijun <tanerivince@gmail.com>
2023-05-16 23:58:56 +0800

    Solve Leetcode 58

---
+++ leetcode/000058.length-of-last-word.ts
@@ -0,0 +1,13 @@
+function lengthOfLastWord(s: string): number {
+	const str = s.trimEnd()
+	let res = 0
+
+	for (let i = str.length - 1; i >= 0; i--) {
+		if (str[i] === " ") {
+			break
+		}
+		res++
+	}
+
+	return res
+}
