6815ef7335469263e1b3d659cd90628c2fe26d08
tanerijun <tanerivince@gmail.com>
2023-05-25 13:39:00 +0800

    Solve Leetcode 27

---
+++ leetcode/000027.remove-element.ts
@@ -0,0 +1,19 @@
+function removeElement(nums: number[], val: number): number {
+	let i = 0
+	let j = nums.length - 1
+
+	while (i <= j) {
+		if (nums[i] === val) {
+			const temp = nums[i]
+			nums[i] = nums[j]
+			nums[j] = temp
+			j--
+		} else {
+			i++
+		}
+	}
+
+	return i
+}
+// Time complexity: O(n)
+// Space compolexity: O(1)
