ad1e1037501d6b06601be426eb4f82eb8c1e9ced
tanerijun <tanerivince@gmail.com>
2023-05-31 00:21:18 +0800

    Solve Leetcode 169 (alt)

--- leetcode/000169.majority-element.ts
+++ leetcode/000169.majority-element.ts
@@ -16,3 +16,27 @@ function majorityElement(nums: number[]): number {
 }
 // Time complexity: O(n)
 // Space complexity: O(n)
+
+// Boyer-Moore algorithm
+function majorityElement2(nums: number[]): number {
+	let curCandidate = nums[0]
+	let count = 1
+
+	for (let i = 1; i < nums.length; i++) {
+		if (nums[i] === curCandidate) {
+			count++
+		} else {
+			count--
+		}
+
+		if (count === 0) {
+			i++
+			curCandidate = nums[i]
+			count = 1
+		}
+	}
+
+	return curCandidate
+}
+// Time complexity: O(n)
+// Space complexity: O(1)
