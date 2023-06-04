2232459ddb594e51a52806d09f5a929b97b2a9bf
tanerijun <tanerivince@gmail.com>
2023-06-04 13:20:59 +0800

    Solve Leetcode 724

---
+++ leetcode/000724.find-pivot-index.ts
@@ -0,0 +1,25 @@
+function pivotIndex(nums: number[]): number {
+	let res = -1
+	const map = new Map<number, number>()
+
+	for (let i = 0; i < nums.length; i++) {
+		const previousSum = map.get(i - 1) ?? 0
+		map.set(i, nums[i] + previousSum)
+	}
+
+	for (let i = 0; i < nums.length; i++) {
+		const leftSum = map.get(i - 1) ?? 0
+		const totalSum = map.get(nums.length - 1) as number
+		const sumUpToIdx = map.get(i) as number
+		const rightSum = totalSum - sumUpToIdx
+
+		if (leftSum === rightSum) {
+			res = i
+			break
+		}
+	}
+
+	return res
+}
+// Time complexity: O(n)
+// Space complexity: O(n)
