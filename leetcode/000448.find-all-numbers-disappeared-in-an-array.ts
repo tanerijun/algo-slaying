f12ff0d24d8e540d2b6f3cb26816998ac0523f5b
tanerijun <tanerivince@gmail.com>
2023-06-08 19:56:00 +0800

    Solve Leetode 448 (alt)

--- leetcode/000448.find-all-numbers-disappeared-in-an-array.ts
+++ leetcode/000448.find-all-numbers-disappeared-in-an-array.ts
@@ -16,3 +16,30 @@ function findDisappearedNumbers(nums: number[]): number[] {
 }
 // Time complexity: O(n)
 // Space complexity: O(n)
+
+function findDisappearedNumbers2(nums: number[]): number[] {
+	const disappearedNumbers: number[] = []
+
+	// Instead of using a set, we can modify the array in place
+	// The input array nums: 1..n map to index 0..n-1 in the array; It's a -1 relationship
+	// Whenever we find a number, we can mark nums[nums[i] - 1] as negative
+	// And we return positive num in nums
+
+	function toNegative(n: number) {
+		return n <= 0 ? n : n * -1
+	}
+
+	for (let i = 0; i < nums.length; i++) {
+		nums[Math.abs(nums[i]) - 1] = toNegative(nums[Math.abs(nums[i]) - 1])
+	}
+
+	for (let i = 0; i <= nums.length; i++) {
+		if (nums[i] > 0) {
+			disappearedNumbers.push(i + 1)
+		}
+	}
+
+	return disappearedNumbers
+}
+// Time complexity: O(n)
+// Space complexity: O(n)
