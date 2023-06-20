2d7506cb5e07d3f3d50b7f3c432c040b1a1d158c
tanerijun <tanerivince@gmail.com>
2023-06-20 20:25:50 +0800

    Solve Leetcode 912

---
+++ leetcode/000912.sort-an-array.ts
@@ -0,0 +1,43 @@
+function sortArray(nums: number[]): number[] {
+	return mergeSort(nums)
+}
+
+function mergeSort(arr: number[]) {
+	if (arr.length === 1) {
+		return arr
+	}
+
+	const mid = Math.floor(arr.length / 2)
+	const leftSide = mergeSort(arr.slice(0, mid))
+	const rightSide = mergeSort(arr.slice(mid))
+	return merge(leftSide, rightSide)
+}
+
+function merge(leftSide: number[], rightSide: number[]): number[] {
+	let l = 0
+	let r = 0
+	let res: number[] = []
+
+	while (l < leftSide.length && r < rightSide.length) {
+		if (leftSide[l] <= rightSide[r]) {
+			res.push(leftSide[l])
+			l++
+		} else {
+			res.push(rightSide[r])
+			r++
+		}
+	}
+
+	if (l < leftSide.length) {
+		res = res.concat(leftSide.slice(l))
+	}
+
+	if (r < rightSide.length) {
+		res = res.concat(rightSide.slice(r))
+	}
+
+	return res
+}
+
+// Time complexity: O(nlog(n))
+// Space complexity: O(n)
