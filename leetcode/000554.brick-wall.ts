7a0ad539b3fd51e64c470c3c5385406845c35b61
tanerijun <tanerivince@gmail.com>
2023-06-22 13:38:18 +0800

    Solve Leetcode 554

---
+++ leetcode/000554.brick-wall.ts
@@ -0,0 +1,24 @@
+function leastBricks(wall: number[][]): number {
+	// The ideal place to draw a line that crossed the least amount of block is after
+	// the common prefix sum with the highest count.
+	const prefixSumCount = new Map<number, number>()
+
+	for (const bricks of wall) {
+		let sum = 0
+
+		// We can skip the last one, because it's the edge
+		for (let i = 0; i < bricks.length - 1; i++) {
+			sum += bricks[i]
+			prefixSumCount.set(sum, (prefixSumCount.get(sum) ?? 0) + 1)
+		}
+	}
+
+	const mapValues = Array.from(prefixSumCount.values())
+
+	return mapValues.length > 0
+		? wall.length - Math.max(...mapValues)
+		: wall.length
+}
+
+// Time complexity: O(n * m)  n = number of rows in the wall, m = width of the wall
+// Space complexity: O(m) m = width of the wall
