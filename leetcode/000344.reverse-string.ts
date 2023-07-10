280b536e632cceda94895022512203b2bdefe101
tanerijun <tanerivince@gmail.com>
2023-07-10 18:32:15 +0800

    Solve Leetcode 344

---
+++ leetcode/000344.reverse-string.ts
@@ -0,0 +1,13 @@
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+	for (let i = 0; i < Math.floor(s.length / 2); i++) {
+		const temp = s[i]
+		s[i] = s[s.length - 1 - i]
+		s[s.length - 1 - i] = temp
+	}
+}
+
+// Time complexity: O(n)
+// Space complexity: O(1)
