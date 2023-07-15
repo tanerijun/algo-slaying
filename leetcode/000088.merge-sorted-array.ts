2c3d3cc5cb417621d627ef8b1f49066a7efdd80b
tanerijun <tanerivince@gmail.com>
2023-07-15 17:57:48 +0800

    Solve Leetcode 88

---
+++ leetcode/000088.merge-sorted-array.ts
@@ -0,0 +1,25 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+	let putIndex = nums1.length - 1
+
+	while (m > 0 && n > 0) {
+		if (nums1[m - 1] > nums2[n - 1]) {
+			nums1[putIndex] = nums1[m - 1]
+			m--
+		} else {
+			nums1[putIndex] = nums2[n - 1]
+			n--
+		}
+
+		putIndex--
+	}
+
+	// Leftovers
+	while (n > 0) {
+		nums1[putIndex] = nums2[n - 1]
+		n--
+		putIndex--
+	}
+}
