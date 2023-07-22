f0c96828a8244014e2f626634f50529716946331
tanerijun <tanerivince@gmail.com>
2023-07-22 13:17:42 +0800

    Solve Leetcode 167

---
+++ leetcode/000167.two-sum-ii-input-array-is-sorted.ts
@@ -0,0 +1,20 @@
+function twoSum(numbers: number[], target: number): number[] {
+	let l = 0
+	let r = numbers.length - 1
+
+	while (l < r) {
+		const sum = numbers[l] + numbers[r]
+
+		if (sum > target) {
+			r--
+		} else if (sum < target) {
+			l++
+		} else {
+			return [l + 1, r + 1]
+		}
+	}
+
+	return []
+}
+// Time complexity: O(n)
+// Space complexity: O(1)
