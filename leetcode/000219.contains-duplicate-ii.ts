0d4a375a3a13ab2a747c5280caf9223996d7177e
tanerijun <tanerivince@gmail.com>
2023-07-26 16:52:27 +0800

    Solve Leetcode 219

--- leetcode/000219.contains-duplicate-ii.ts
+++ leetcode/000219.contains-duplicate-ii.ts
@@ -1,1 +1,15 @@
-function containsNearbyDuplicate(nums: number[], k: number): boolean {}
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+	const map = new Map<number, number>()
+
+	for (let i = 0; i < nums.length; i++) {
+		const prevIndex = map.get(nums[i])
+		if (prevIndex !== undefined && i - prevIndex <= k) {
+			return true
+		}
+		map.set(nums[i], i)
+	}
+
+	return false
+}
+// Time complexity: O(n)
+// Space complexity: O(n)
