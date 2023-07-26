70e37c9d0b3aeef5a4b3d6499f2b80e47dec405f
tanerijun <tanerivince@gmail.com>
2023-07-26 13:02:18 +0800

    Solve Leetcode 881

---
+++ leetcode/000881.boats-to-save-people.ts
@@ -0,0 +1,25 @@
+function numRescueBoats(people: number[], limit: number): number {
+	people.sort((a, b) => a - b)
+
+	let boatCount = 0
+	let currentLimit = limit
+	let l = 0
+	let r = people.length - 1
+
+	while (l <= r) {
+		boatCount++
+		currentLimit -= people[r]
+
+		if (currentLimit > 0 && currentLimit - people[l] >= 0) {
+			currentLimit -= people[l]
+			l++
+		}
+
+		r--
+		currentLimit = limit
+	}
+
+	return boatCount
+}
+// Time complexity: O(n)
+// Space complexity: O(1)
