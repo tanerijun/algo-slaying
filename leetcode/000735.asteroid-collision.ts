0b78ac3881082e9825abcc5d64c5fd00cc925369
tanerijun <tanerivince@gmail.com>
2023-08-19 17:03:45 +0800

    Solve Leetcode 735

---
+++ leetcode/000735.asteroid-collision.ts
@@ -0,0 +1,26 @@
+function asteroidCollision(asteroids: number[]): number[] {
+	const stack: number[] = [];
+
+	for (let asteroid of asteroids) {
+		while (stack && stack[stack.length - 1] > 0 && asteroid < 0) {
+			const sizeAfterCollision = stack[stack.length - 1] + asteroid;
+
+			if (sizeAfterCollision < 0) {
+				stack.pop();
+			} else if (sizeAfterCollision > 0) {
+				asteroid = 0;
+			} else {
+				stack.pop();
+				asteroid = 0;
+			}
+		}
+
+		if (asteroid) {
+			stack.push(asteroid);
+		}
+	}
+
+	return stack;
+}
+// Time complexity: O(n)
+// Space complexity: O(n)
