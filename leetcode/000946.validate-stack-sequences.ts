4aa3a2af4143792f741cd2405225aee7260e931f
tanerijun <tanerivince@gmail.com>
2023-08-20 13:26:32 +0800

    Solve Leetcode 946

---
+++ leetcode/000946.validate-stack-sequences.ts
@@ -0,0 +1,15 @@
+function validateStackSequences(pushed: number[], popped: number[]): boolean {
+	const stack: number[] = [];
+
+	let toPopIdx = 0;
+	for (const toPush of pushed) {
+		stack.push(toPush);
+
+		while (stack && toPopIdx < popped.length && stack[stack.length - 1] === popped[toPopIdx]) {
+			stack.pop();
+			toPopIdx++;
+		}
+	}
+
+	return stack.length === 0;
+}
