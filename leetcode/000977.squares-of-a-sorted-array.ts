b5b076351b29d64c09fded838434afc2851d7d61
tanerijun <tanerivince@gmail.com>
2023-08-29 23:45:16 +0800

    Solve Leetcode 977

---
+++ leetcode/000977.squares-of-a-sorted-array.ts
@@ -0,0 +1,19 @@
+function sortedSquares(nums: number[]): number[] {
+	const ans: number[] = Array(nums.length).fill(0);
+	let l = 0;
+	let r = nums.length - 1;
+	let i = nums.length - 1;
+	while (i >= 0) {
+		if (nums[r] > Math.abs(nums[l])) {
+			ans[i] = nums[r] * nums[r];
+			r--;
+		} else {
+			ans[i] = nums[l] * nums[l];
+			l++;
+		}
+		i--;
+	}
+	return ans;
+}
+// Time complexity: O(n)
+// Space complexity: O(1)
