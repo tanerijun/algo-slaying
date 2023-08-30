8e9b4d18f68f683f8d8e28146c75bbdfd6a56682
tanerijun <tanerivince@gmail.com>
2023-08-30 16:27:04 +0800

    Solve Leetcode 69

---
+++ leetcode/000069.sqrtx.ts
@@ -0,0 +1,21 @@
+function mySqrt(x: number): number {
+	if (x === 0) return 0;
+	let l = 1;
+	let r = x;
+	while (l < r) {
+		const m = Math.ceil((l + r) / 2);
+		const mSquared = m * m;
+		if (mSquared > x) {
+			r = m - 1;
+		} else if (mSquared < x) {
+			l = m + 1;
+		} else {
+			return m;
+		}
+	}
+
+	const lSquared = l * l;
+	return lSquared === x ? l : lSquared > x ? l - 1 : l;
+}
+
+console.log(mySqrt(6));
