05644011531dc7b5a4ad47441eab808d4f52aaf9
tanerijun <tanerivince@gmail.com>
2023-08-30 16:31:21 +0800

    Solve Leetcode 367

---
+++ leetcode/000367.valid-perfect-square.ts
@@ -0,0 +1,18 @@
+function isPerfectSquare(num: number): boolean {
+	let l = 0;
+	let r = num;
+	while (l <= r) {
+		const m = Math.floor((l + r) / 2);
+		const mSquared = m * m;
+		if (mSquared === num) {
+			return true;
+		} else if (mSquared > num) {
+			r = m - 1;
+		} else {
+			l = m + 1;
+		}
+	}
+	return false;
+}
+// Time complexity: O(n)
+// Space complexity: O(1)
