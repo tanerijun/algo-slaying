03560b5411c61a814959cedc8c50f245074a367c
tanerijun <tanerivince@gmail.com>
2023-09-03 13:39:31 +0800

    Solve Leetcode 74

---
+++ leetcode/000074.search-a-2d-matrix.ts
@@ -0,0 +1,27 @@
+function searchMatrix(matrix: number[][], target: number): boolean {
+	let rows = matrix.length;
+	let cols = matrix[0].length;
+	let total = rows * cols;
+
+	let l = 0;
+	let r = total - 1;
+
+	while (l <= r) {
+		let m = Math.floor((l + r) / 2);
+		// To find out in which row the nth element is, m / number of elements in a row
+		// To find out the in which column it is, use the remainder of the division above
+		let mVal = matrix[Math.floor(m / cols)][m % cols];
+
+		if (mVal > target) {
+			r = m - 1;
+		} else if (mVal < target) {
+			l = m + 1;
+		} else {
+			return true;
+		}
+	}
+
+	return false;
+}
+// Time complexity: O(log(n))
+// Space complexity: O(1)
