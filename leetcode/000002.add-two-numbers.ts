75122480db28355c0a363dbe7e40204827a30a94
tanerijun <tanerivince@gmail.com>
2023-09-14 11:17:49 +0800

    Solve Leetcode 2

---
+++ leetcode/000002.add-two-numbers.ts
@@ -0,0 +1,34 @@
+class ListNode {
+	val: number;
+	next: ListNode | null;
+	constructor(val?: number, next?: ListNode | null) {
+		this.val = val === undefined ? 0 : val;
+		this.next = next === undefined ? null : next;
+	}
+}
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+	const answerHead = new ListNode();
+	let cursor = answerHead;
+	let carry = 0;
+
+	while (l1 !== null || l2 !== null) {
+		const x = l1 ? l1.val : 0;
+		const y = l2 ? l2.val : 0;
+		const sum = x + y + carry;
+		carry = sum >= 10 ? 1 : 0;
+		cursor.next = new ListNode(sum % 10);
+		cursor = cursor.next;
+
+		if (l1 != null) l1 = l1.next;
+		if (l2 != null) l2 = l2.next;
+	}
+
+	if (carry > 0) {
+		cursor.next = new ListNode(carry);
+	}
+
+	return answerHead.next;
+}
+// Time complexity: O(m + n)
+// Space complexity: O(1)
