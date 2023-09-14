6083f394a597e74d9e9b44bfb9b1b07450f5e09e
tanerijun <tanerivince@gmail.com>
2023-09-14 18:42:47 +0800

    Solve Leetcode 707

---
+++ leetcode/000707.design-linked-list.ts
@@ -0,0 +1,87 @@
+class LinkedListItem {
+	constructor(public readonly val: number, public next?: LinkedListItem) {}
+}
+
+class MyLinkedList {
+	private head?: LinkedListItem;
+
+	get(index: number): number {
+		let item = this.head;
+
+		for (let i = 0; i <= index && item; i++) {
+			if (i === index) {
+				return item.val;
+			}
+
+			item = item.next;
+		}
+
+		return -1;
+	}
+
+	addAtHead(val: number): void {
+		this.head = new LinkedListItem(val, this.head);
+	}
+
+	addAtTail(val: number): void {
+		if (!this.head) {
+			return this.addAtHead(val);
+		}
+
+		let lastItem = this.head;
+
+		while (lastItem.next) {
+			lastItem = lastItem.next;
+		}
+
+		lastItem.next = new LinkedListItem(val);
+	}
+
+	addAtIndex(index: number, val: number): void {
+		if (!index) {
+			return this.addAtHead(val);
+		}
+
+		let item = this.head;
+
+		for (let i = 0; i <= index - 1 && item; i++) {
+			if (i === index - 1) {
+				item.next = new LinkedListItem(val, item.next);
+
+				return;
+			}
+
+			item = item.next;
+		}
+	}
+
+	deleteAtIndex(index: number): void {
+		if (!index) {
+			this.head = this.head?.next;
+
+			return;
+		}
+
+		let item = this.head;
+
+		for (let i = 0; i <= index - 1 && item; i++) {
+			if (i === index - 1) {
+				item.next = item.next?.next;
+
+				return;
+			}
+
+			item = item.next;
+		}
+	}
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
