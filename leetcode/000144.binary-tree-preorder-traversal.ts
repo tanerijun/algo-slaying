1df1f2b6841c01af265f013160582f949d3acedd
tanerijun <tanerivince@gmail.com>
2023-10-05 21:03:22 +0800

    Solve Leetcode 144

--- leetcode/000144.binary-tree-preorder-traversal.ts
+++ leetcode/000144.binary-tree-preorder-traversal.ts
@@ -27,3 +27,27 @@ function preorderTraversal(root: TreeNode | null): number[] {
 }
 // Time complexity: O(n)
 // Space complexity: O(n)
+
+// Iterative with stack
+function preorderTraversalIterative(root: TreeNode | null): number[] {
+	const res: number[] = []
+	const stack: TreeNode[] = []
+	let cur = root
+
+	while (cur || stack.length > 0) {
+		while (cur) {
+			if (cur.right) {
+				stack.push(cur.right)
+			}
+
+			res.push(cur.val)
+			cur = cur.left
+		}
+
+		cur = stack.pop()!
+	}
+
+	return res
+}
+// Time complexity: O(n)
+// Space complexity: O(n)
