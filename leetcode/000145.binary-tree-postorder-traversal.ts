1ae5c72a4ec4a7afbed836bae3ba80858941f9ce
tanerijun <tanerivince@gmail.com>
2023-10-11 12:26:40 +0800

    Solve Leetcode 145

--- leetcode/000145.binary-tree-postorder-traversal.ts
+++ leetcode/000145.binary-tree-postorder-traversal.ts
@@ -27,3 +27,34 @@ function postorderTraversal(root: TreeNode | null): number[] {
 }
 // Time complexity: O(n)
 // Space complexity: O(n)
+
+// Iterative with stack
+function postorderTraversalIterative(root: TreeNode | null): number[] {
+	const res: number[] = []
+	const stack: Array<TreeNode | null> = [root]
+	const visited: boolean[] = [false]
+
+	while (stack.length > 0) {
+		const cur = stack.pop()
+		const isVisited = visited.pop()
+
+		if (cur) {
+			if (isVisited) {
+				res.push(cur.val)
+			} else {
+				stack.push(cur)
+				visited.push(true)
+
+				stack.push(cur.right)
+				visited.push(false)
+
+				stack.push(cur.left)
+				visited.push(false)
+			}
+		}
+	}
+
+	return res
+}
+// Time complexity: O(n)
+// Space complexity: O(n)
