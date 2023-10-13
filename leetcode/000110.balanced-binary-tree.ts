e108f77115033997c57d76e1e66b056296699c27
tanerijun <tanerivince@gmail.com>
2023-10-13 17:04:27 +0800

    Solve Leetcode 110

---
+++ leetcode/000110.balanced-binary-tree.ts
@@ -0,0 +1,30 @@
+class TreeNode {
+	val: number
+	left: TreeNode | null
+	right: TreeNode | null
+	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+		this.val = val === undefined ? 0 : val
+		this.left = left === undefined ? null : left
+		this.right = right === undefined ? null : right
+	}
+}
+
+function isBalanced(root: TreeNode | null): boolean {
+	// Return a tuple: (isBalanced, height)
+	function dfs(root: TreeNode | null): [boolean, number] {
+		if (!root) {
+			return [true, 0]
+		}
+
+		const left = dfs(root.left)
+		const right = dfs(root.right)
+		const heightDiff = Math.abs(left[1] - right[1])
+		const isBalanced = left[0] && right[0] && heightDiff <= 1
+
+		return [isBalanced, 1 + Math.max(left[1], right[1])]
+	}
+
+	return dfs(root)[0]
+}
+// Time complexity: O(n)
+// Space complexity: O(n)
