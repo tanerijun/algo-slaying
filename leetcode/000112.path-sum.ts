3c1cc8b2198304881b7b3313563e959381478dd2
tanerijun <tanerivince@gmail.com>
2023-10-19 17:59:25 +0800

    Solve Leetcode 112

---
+++ leetcode/000112.path-sum.ts
@@ -0,0 +1,30 @@
+class TreeNode {
+	val: number
+	left: TreeNode | null
+	right: TreeNode | null
+	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+		this.val = val === undefined ? 0 : val
+		this.left = left === undefined ? null : left
+		this.right = right === undefined ? null : right
+	}
+}
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+	function helper(root: TreeNode | null, currentSum: number) {
+		if (!root) {
+			return false
+		}
+
+		const sum = currentSum + root.val
+
+		if (!root.left && !root.right) {
+			return sum === targetSum
+		}
+
+		return helper(root.left, sum) || helper(root.right, sum)
+	}
+
+	return helper(root, 0)
+}
+// Time complexity: O(n)
+// Space complexity: O(n)
