5f1ae95c2e72be80d5d9df7d34d7190fd71d05ea
tanerijun <tanerivince@gmail.com>
2023-10-20 13:29:27 +0800

    Solve Leetcode 606

---
+++ leetcode/000606.construct-string-from-binary-tree.ts
@@ -0,0 +1,28 @@
+class TreeNode {
+	val: number
+	left: TreeNode | null
+	right: TreeNode | null
+	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+		this.val = val === undefined ? 0 : val
+		this.left = left === undefined ? null : left
+		this.right = right === undefined ? null : right
+	}
+}
+
+function tree2str(root: TreeNode | null): string {
+	if (!root) {
+		return ""
+	}
+
+	let leftChildStr = tree2str(root.left)
+	let rightChildStr = tree2str(root.right)
+
+	if (leftChildStr || rightChildStr) {
+		leftChildStr = `(${leftChildStr})`
+		rightChildStr = rightChildStr && `(${rightChildStr})`
+	}
+
+	return root.val + leftChildStr + rightChildStr
+}
+// Time complexity: O(n)
+// Space complexity: O(n)
