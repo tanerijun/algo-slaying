2e566e216a2884c4dc209be6062935f13012321d
tanerijun <tanerivince@gmail.com>
2023-12-08 21:30:00 +0800

    Solve aoc-d1-p1

--- advent-of-code/2023/day1/part1.ts
+++ advent-of-code/2023/day1/part1.ts
@@ -1,3 +1,43 @@
-import * as fs from "node:fs"
+import { createReadStream } from "node:fs"
+import { createInterface } from "node:readline"
 
-console.log(fs)
+const filePath = process.argv[2]
+if (!filePath) {
+	console.log("Usage: npx tsx path-to-script path-to-input")
+	process.exit(1)
+}
+
+const fileStream = createReadStream(filePath)
+const rl = createInterface({ input: fileStream, crlfDelay: Infinity })
+
+function isNumeric(x: string) {
+	return /\d/.test(x)
+}
+
+const nums: Array<number> = []
+
+for await (const line of rl) {
+	let firstNum = ""
+	let secondNum = ""
+
+	for (let i = 0; i < line.length; i++) {
+		const curChar = line[i] as string
+		if (isNumeric(curChar)) {
+			firstNum = curChar
+			break
+		}
+	}
+
+	for (let i = line.length - 1; i >= 0; i--) {
+		const curChar = line[i] as string
+		if (isNumeric(curChar)) {
+			secondNum = curChar
+			break
+		}
+	}
+
+	nums.push(parseInt(firstNum + secondNum))
+}
+
+const sum = nums.reduce((a, b) => a + b)
+console.log(sum)
