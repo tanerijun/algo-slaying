2beb5f4b35a1c8b4b3f25b48858589837b81b918
tanerijun <tanerivince@gmail.com>
2024-01-27 23:05:01 +0800

    Add Go Two Sum

---
+++ leetcode/go/000001.two-sum.go
@@ -0,0 +1,15 @@
+package leetcode
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		if complement, ok := m[target-nums[i]]; ok {
+			return []int{complement, i}
+		}
+		m[nums[i]] = i
+	}
+
+	return nil
+}
