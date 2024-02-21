b825d91f51aa94344b6d4da663f1f4f23300bb01
tanerijun <tanerivince@gmail.com>
2024-02-21 23:30:43 +0800

    Refactor Go Leetcode 2

--- leetcode/go/000002.add-two-numbers.go
+++ leetcode/go/000002.add-two-numbers.go
@@ -4,41 +4,25 @@ package leetcode
 // Space complexity: O(n)
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	dummyHead := &ListNode{}
-	res := dummyHead
-	cur1 := l1
-	cur2 := l2
-	carry := 0
+	res, cur1, cur2, carry := dummyHead, l1, l2, 0
 
-	for cur1 != nil || cur2 != nil {
+	for cur1 != nil || cur2 != nil || carry != 0 {
 		val1 := 0
 		if cur1 != nil {
 			val1 = cur1.Val
+			cur1 = cur1.Next
 		}
 		val2 := 0
 		if cur2 != nil {
 			val2 = cur2.Val
-		}
-		sum := val1 + val2 + carry
-		if sum >= 10 {
-			carry = 1
-			sum -= 10
-		} else {
-			carry = 0
-		}
-
-		res.Next = &ListNode{Val: sum}
-		res = res.Next
-
-		if cur1 != nil {
-			cur1 = cur1.Next
-		}
-		if cur2 != nil {
 			cur2 = cur2.Next
 		}
-	}
 
-	if carry == 1 {
-		res.Next = &ListNode{Val: 1}
+		sum := val1 + val2 + carry
+		carry = sum / 10
+
+		res.Next = &ListNode{Val: sum % 10}
+		res = res.Next
 	}
 
 	return dummyHead.Next
