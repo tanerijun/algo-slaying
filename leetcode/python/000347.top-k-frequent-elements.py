5d363e52dbbb2033bc00e8c72ca4d16bd87dc674
tanerijun <tanerivince@gmail.com>
2024-04-24 23:48:27 +0800

    Solve Leetcode 347

--- leetcode/python/000347.top-k-frequent-elements.py
+++ leetcode/python/000347.top-k-frequent-elements.py
@@ -13,3 +13,24 @@ class Solution:
             res.append(items[i][0])
 
         return res
+
+
+class Solution2:
+    # Using a variation of bucket sort
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        count: dict[int, int] = {}
+        freq: list[list[int]] = [[] for _ in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = count.get(n, 0) + 1
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res: list[int] = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
