904673937872ac15a86b6b9e4e7127c575ffa4a7
tanerijun <tanerivince@gmail.com>
2024-05-09 22:17:47 +0800

    Product of Array Except Self

--- leetcode/python/000238.product-of-array-except-self.py
+++ leetcode/python/000238.product-of-array-except-self.py
@@ -1,13 +1,15 @@
 class Solution:
-	# Time complexity: O(n)
-	# Space complexity: O(n)
+    # Time complexity: O(n)
+    # Space complexity: O(n)
     def productExceptSelf(self, nums: list[int]) -> list[int]:
         prefix_products = [0] * len(nums)
         suffix_products = [0] * len(nums)
         for i in range(len(nums)):
             prefix_products[i] = nums[i] * prefix_products[i - 1] if i > 0 else nums[i]
         for i in range(len(nums) - 1, 0, -1):
-            suffix_products[i] = nums[i] * suffix_products[i + 1] if i < len(nums) - 1 else nums[i]
+            suffix_products[i] = (
+                nums[i] * suffix_products[i + 1] if i < len(nums) - 1 else nums[i]
+            )
 
         res = []
         for i in range(len(nums)):
@@ -18,23 +20,22 @@ class Solution:
 
         return res
 
+
 # Solution1 optimized, since output array doesn't count as extra memory
 class Solution2:
-	# Time complexity: O(n)
-	# Space complexity: O(1)
+    # Time complexity: O(n)
+    # Space complexity: O(1)
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        prefix_products = [0] * len(nums)
-        suffix_products = [0] * len(nums)
-        for i in range(len(nums)):
-            prefix_products[i] = nums[i] * prefix_products[i - 1] if i > 0 else nums[i]
-        for i in range(len(nums) - 1, 0, -1):
-            suffix_products[i] = nums[i] * suffix_products[i + 1] if i < len(nums) - 1 else nums[i]
+        res = [1] * len(nums)
 
-        res = []
+        prefix = 1
         for i in range(len(nums)):
-            prefix = 1 if i == 0 else prefix_products[i - 1]
-            postfix = 1 if i == len(nums) - 1 else suffix_products[i + 1]
-            print(prefix, postfix)
-            res.append(prefix * postfix)
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
 
         return res
