48a8bc026560b747ae66147f76edc0167a9d1fb6
tanerijun <tanerivince@gmail.com>
2024-05-19 13:40:53 +0800

    Solve Leetcode 853

--- leetcode/python/000853.car-fleet.py
+++ leetcode/python/000853.car-fleet.py
@@ -14,3 +14,19 @@ class Solution:
 				res += 1
 
 		return res
+
+	# Time complexity: O(n)
+	# Space complexity: O(n) - slightly more space efficient then carFleet
+	def carFleet2(self, target: int, position: list[int], speed: list[int]) -> int:
+		# Sort the indices based on position in descending order
+		sorted_indices = sorted(range(len(position)), key=lambda i: -position[i])
+
+		res = 0
+		blocker_time_to_target = 0
+
+		for i in sorted_indices:
+			time_to_target = (target - position[i]) / speed[i]
+			if time_to_target > blocker_time_to_target:
+				blocker_time_to_target = time_to_target
+				res += 1
+		return res
