8efc56da215092d787e058f863bdabd0a2f37bb1
tanerijun <tanerivince@gmail.com>
2024-05-29 20:27:25 +0800

    Add description

--- apcs/兩點距離.cpp
+++ apcs/兩點距離.cpp
@@ -1,3 +1,17 @@
+// Content
+// 請設計一個函式，輸入兩點(x1,y1)及(x2,y2)的座標，傳回兩點的距離
+
+// Input
+// 輸入四個浮點數x1, y1, x2, y2，分別為兩個點的座標
+
+// Output
+// 輸出兩點間的距離
+
+// Sample Input #1
+// 1.0 2.0 4.0 6.0
+// Sample Output #1
+// 5
+
 #include <cmath>
 #include <iostream>
 
