b51167e9f9b9b2ea3e19b41baa37f5f27fb647f6
tanerijun <tanerivince@gmail.com>
2024-06-04 21:11:56 +0800

    Simplify code

--- leetcode/python/000146.lru-cache.py
+++ leetcode/python/000146.lru-cache.py
@@ -1,74 +1,131 @@
 from typing import Optional
 
+# Initial version: a bit convoluted
 
+# class Node:
+#     def __init__(
+#         self,
+#         value: tuple[int, int] = (-1, -1),  # contains (key, value)
+#         prev: Optional["Node"] = None,
+#         next: Optional["Node"] = None,
+#     ):
+#         self.value = value
+#         self.prev = prev
+#         self.next = next
+
+# class LRUCache:
+#     def __init__(self, capacity: int):
+#         self.capacity = capacity
+#         self.map = {}  # map key to Node
+#         self.leftDummy = Node()  # point right to least recently used
+#         self.rightDummy = Node()  # point left to most recently used
+#         self.leftDummy.next = self.rightDummy
+#         self.rightDummy.prev = self.leftDummy
+
+#     def _insert_most_recent(self, node: Node) -> None:
+#         node.prev = self.rightDummy.prev
+#         node.next = self.rightDummy
+#         self.rightDummy.prev.next = node
+#         self.rightDummy.prev = node
+
+#     def _delete_node(self, node: Node) -> None:
+#         node.prev.next = node.next
+#         node.next.prev = node.prev
+#         node.prev = None
+#         node.next = None
+
+#     def _update_most_recent(self, node: Node) -> None:
+#         self._delete_node(node)
+#         self._insert_most_recent(node)
+
+#     def get(self, key: int) -> int:
+#         if not key in self.map:
+#             return -1
+
+#         node = self.map[key]
+#         self._update_most_recent(node)
+#         return node.value[1]
+
+#     def put(self, key: int, value: int) -> None:
+#         if key in self.map:
+#             node = self.map[key]
+#             node.value = (key, value)
+#             self._update_most_recent(node)
+#             return
+
+#         if len(self.map) + 1 > self.capacity:
+#             node = self.leftDummy.next
+#             self._delete_node(node)
+#             del self.map[node.value[0]]
+
+#         node = Node((key, value))
+#         self.map[key] = node
+#         self._insert_most_recent(node)
+
+#     def print_cache(self) -> str:
+#         res = []
+#         for k, v in self.map.items():
+#             res.append(f"{k} => {v.value[1]}")
+
+#         return ", ".join(res)
+
+
+# Simplified and cleaned
 class Node:
     def __init__(
         self,
-        value: tuple[int, int] = (-1, -1),  # contains (key, value)
+        key: int,
+        val: int,
         prev: Optional["Node"] = None,
         next: Optional["Node"] = None,
     ):
-        self.value = value
+        self.key = key
+        self.val = val
         self.prev = prev
         self.next = next
 
 
 class LRUCache:
-
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.map = {}  # map key to Node
-        self.leftDummy = Node()  # point right to least recently used
-        self.rightDummy = Node()  # point left to most recently used
+        self.leftDummy = Node(-1, -1)  # points right to least recently used node
+        self.rightDummy = Node(-1, -1)  # points left to most recently used node
         self.leftDummy.next = self.rightDummy
         self.rightDummy.prev = self.leftDummy
 
-    def _insert_most_recent(self, node: Node) -> None:
-        node.prev = self.rightDummy.prev
+    def _insert(self, node: Node):
         node.next = self.rightDummy
+        node.prev = self.rightDummy.prev
         self.rightDummy.prev.next = node
         self.rightDummy.prev = node
 
-    def _delete_node(self, node: Node) -> None:
+    def _delete(self, node: Node):
         node.prev.next = node.next
         node.next.prev = node.prev
-        node.prev = None
-        node.next = None
-
-    def _update_most_recent(self, node: Node) -> None:
-        self._delete_node(node)
-        self._insert_most_recent(node)
 
     def get(self, key: int) -> int:
         if not key in self.map:
             return -1
 
         node = self.map[key]
-        self._update_most_recent(node)
-        return node.value[1]
+        self._delete(node)
+        self._insert(node)
+        return node.val
 
     def put(self, key: int, value: int) -> None:
         if key in self.map:
             node = self.map[key]
-            node.value = (key, value)
-            self._update_most_recent(node)
-            return
+            self._delete(node)
 
-        if len(self.map) + 1 > self.capacity:
-            node = self.leftDummy.next
-            self._delete_node(node)
-            del self.map[node.value[0]]
-
-        node = Node((key, value))
+        node = Node(key, value)
         self.map[key] = node
-        self._insert_most_recent(node)
+        self._insert(node)
 
-    def print_cache(self) -> str:
-        res = []
-        for k, v in self.map.items():
-            res.append(f"{k} => {v.value[1]}")
-
-        return ", ".join(res)
+        if len(self.map) > self.capacity:
+            lru = self.leftDummy.next
+            self._delete(lru)
+            del self.map[lru.key]
 
 
 # Your LRUCache object will be instantiated and called as such:
@@ -76,6 +133,7 @@ class LRUCache:
 # param_1 = obj.get(key)
 # obj.put(key,value)
 
+# For testing purposes
 # res = []
 # cache = LRUCache(2)
 # res.append(cache.put(2, 1))
