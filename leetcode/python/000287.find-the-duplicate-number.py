dada9cd30bc1126f64ff82bf1d6f9d4b30ddf170
tanerijun <tanerivince@gmail.com>
2024-07-08 23:42:04 +0800

    Add comment

--- leetcode/python/000287.find-the-duplicate-number.py
+++ leetcode/python/000287.find-the-duplicate-number.py
@@ -3,6 +3,7 @@
 
 class Solution:
     def findDuplicate(self, nums: list[int]) -> int:
+        # Start a fast and slow pointers until they meet
         slow, fast = 0, 0
         while True:
             slow = nums[slow]
@@ -10,6 +11,8 @@ class Solution:
             if slow == fast:
                 break
 
+        # As soon as they meet, reset one pointer,
+        # and move both pointers at the same speed until they meet again
         finder = 0
         while True:
             slow = nums[slow]
