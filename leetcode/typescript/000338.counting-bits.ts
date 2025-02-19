2dc26c0288ca6fedb104cded109e64fcec0e7648
tanerijun <tanerivince@gmail.com>
2025-02-19 10:19:12 +0800

    Solve Leetcode 338

---
+++ leetcode/typescript/000338.counting-bits.ts
@@ -0,0 +1,33 @@
+// Time complexity: O(nlog(n)) -> log(n) because in general a number i needs [log₂(i)] + 1 bits
+// Space complexity: O(1)
+function countBits(n: number): number[] {
+  const res: number[] = [];
+
+  for (let i = 0; i <= n; i++) {
+    let num = i;
+    let temp = 0;
+    while (num > 0) {
+      temp += num & 1;
+      num >>= 1;
+    }
+    res.push(temp);
+  }
+
+  return res;
+}
+
+// Time complexity: O(n)
+// Space complexity: O(1) -> depends on if we count dp array as extra space or not (it's also the ans array)
+function countBit2(n: number): number[] {
+  if (n === 0) return [0];
+
+  const dp = Array(n).fill(0);
+  let offset = 1; // largest most-significant-bit reached
+
+  for (let i = 1; i <= n; i++) {
+    if (offset * 2 === i) offset = i;
+    dp[i] = 1 + dp[i - offset];
+  }
+
+  return dp;
+}
