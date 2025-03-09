4e7c9eb35fa588a1a2cf8b24efd6f80affc55bba
tanerijun <tanerivince@gmail.com>
2025-03-09 15:15:34 +0800

    Solve Leetcode 994

--- leetcode/typescript/000994.rotting-oranges.ts
+++ leetcode/typescript/000994.rotting-oranges.ts
@@ -47,3 +47,47 @@ function orangesRotting(grid: number[][]): number {
 
   return freshOranges > 0 ? -1 : clock;
 }
+
+// Time complexity: O(m * n)
+// Space complexity: O(m * n)
+function orangesRotting2(grid: number[][]): number {
+  const rows = grid.length;
+  const cols = grid[0].length;
+  const queue = new Queue<[number, number]>();
+  let clock = 0;
+  let freshOranges = 0;
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (grid[r][c] === 0) continue;
+      if (grid[r][c] === 1) freshOranges++;
+      if (grid[r][c] === 2) queue.enqueue([r, c]);
+    }
+  }
+
+  while (!queue.isEmpty()) {
+    const n = queue.size();
+    let rotted = false;
+
+    for (let i = 0; i < n; i++) {
+      const [r, c] = queue.dequeue();
+      for (const [r_off, c_off] of [[-1, 0], [1, 0], [0, -1], [0, 1]]) {
+        const newR = r + r_off;
+        const newC = c + c_off;
+        if (
+          newR >= 0 && newC >= 0 && newR < rows && newC < cols &&
+          grid[newR][newC] === 1
+        ) {
+          rotted = true;
+          freshOranges--;
+          grid[newR][newC] = 2;
+          queue.enqueue([newR, newC]);
+        }
+      }
+    }
+
+    if (rotted) clock++;
+  }
+
+  return freshOranges > 0 ? -1 : clock;
+}
