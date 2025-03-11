c8283ea1826b2141badbe017c86ca867d276267d
tanerijun <tanerivince@gmail.com>
2025-03-11 09:38:51 +0800

    Update Leetcode 130

--- leetcode/typescript/000130.surrounded-regions.ts
+++ leetcode/typescript/000130.surrounded-regions.ts
@@ -19,13 +19,13 @@ function solve(board: string[][]): void {
   }
 
   for (let c = 0; c < cols; c++) {
-    if (board[0][c] === "O") dfs(0, c);
-    if (board[rows - 1][c] === "O") dfs(rows - 1, c);
+    dfs(0, c);
+    dfs(rows - 1, c);
   }
 
   for (let r = 0; r < rows; r++) {
-    if (board[r][0] === "O") dfs(r, 0);
-    if (board[r][cols - 1] === "O") dfs(r, cols - 1);
+    dfs(r, 0);
+    dfs(r, cols - 1);
   }
 
   for (let r = 0; r < rows; r++) {
