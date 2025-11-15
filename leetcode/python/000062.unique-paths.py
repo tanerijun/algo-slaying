570b089a76329caccde2e9f14fcc0d711b671501
tanerijun <tanerivince@gmail.com>
2025-11-15 19:15:00 +0800

    LC 62

---
+++ leetcode/python/000062.unique-paths.py
@@ -0,0 +1,12 @@
+class Solution:
+    # Time complexity: O(m * n)
+    # Space complexity: O(m * n)
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp[m - 1][n - 1] = 1
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                dp[i][j] += dp[i + 1][j] + dp[i][j + 1]
+
+        return dp[0][0]
