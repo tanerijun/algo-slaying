81ff0d8010b6a4b874acaa32fc21497843f9204b
tanerijun <tanerivince@gmail.com>
2025-12-30 11:54:56 +0800

    LC 5

--- leetcode/python/000005.longest-palindromic-substring.py
+++ leetcode/python/000005.longest-palindromic-substring.py
@@ -27,3 +27,24 @@ class Solution:
                 currentMaxLen = length
 
         return s[resStartIndex : resStartIndex + currentMaxLen]
+
+    # Time complexity: O(n2)
+    # Space complexity: O(1)
+    def longestPalindrome2(self, s: str) -> str:
+        def expand_around_center(l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return l + 1, r
+
+        start, end = 0, 0
+        for i in range(len(s)):
+            l1, r1 = expand_around_center(i, i)
+            l2, r2 = expand_around_center(i, i + 1)
+
+            if r1 - l1 > end - start:
+                start, end = l1, r1
+            if r2 - l2 > end - start:
+                start, end = l2, r2
+
+        return s[start:end]
