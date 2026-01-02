51e33299cf00aae13001eb81834831c3f6dff27d
tanerijun <tanerivince@gmail.com>
2026-01-02 10:34:43 +0800

    LC 3110

---
+++ leetcode/python/003110.score-of-a-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+        for i in range(len(s) - 1):
+            score += abs(ord(s[i]) - ord(s[i + 1]))
+        return score
