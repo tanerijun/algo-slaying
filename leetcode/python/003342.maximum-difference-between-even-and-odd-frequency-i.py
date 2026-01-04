d5b41778363d1445cc71e3149a0c58bccd8d9eed
tanerijun <tanerivince@gmail.com>
2026-01-04 14:53:07 +0800

    LC 3342

---
+++ leetcode/python/003342.maximum-difference-between-even-and-odd-frequency-i.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        count = Counter(s)
+        max_odd, min_even = float("-inf"), float("inf")
+        for v in count.values():
+            if v % 2 == 0:
+                min_even = min(min_even, v)
+            else:
+                max_odd = max(max_odd, v)
+        return int(max_odd - min_even)
