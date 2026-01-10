31fcd6c3224301ac8fa8ff63ccac844f4768f840
tanerijun <tanerivince@gmail.com>
2026-01-10 14:56:58 +0800

    LC 3151

--- leetcode/python/003151.special-array-i.py
+++ leetcode/python/003151.special-array-i.py
@@ -21,3 +21,11 @@ class Solution:
             if nums[i - 1] % 2 == nums[i] % 2:
                 return False
         return True
+
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    def isArraySpecial3(self, nums: list[int]) -> bool:
+        for i in range(1, len(nums)):
+            if (nums[i - 1] + nums[i]) % 2 != 1:
+                return False
+        return True
