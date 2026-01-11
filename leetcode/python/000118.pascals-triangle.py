d7d577e1c23b6cf38a036854c411bde8ad72f331
tanerijun <tanerivince@gmail.com>
2026-01-11 14:40:22 +0800

    LC 118

--- leetcode/python/000118.pascals-triangle.py
+++ leetcode/python/000118.pascals-triangle.py
@@ -12,3 +12,17 @@ class Solution:
             res.append(next)
 
         return res
+
+    # Time complexity: O(n2)
+    # Space complexity: O(n)
+    def generate2(self, numRows: int) -> list[list[int]]:
+        res = [[1]]
+
+        for _ in range(numRows - 1):
+            next = [0] * (len(res[-1]) + 1)
+            for i in range(len(res[-1])):
+                next[i] += res[-1][i]
+                next[i + 1] += res[-1][i]
+            res.append(next)
+
+        return res
