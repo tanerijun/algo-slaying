f38f9fc535f2c93f1606fd168ee13dc05f70a81e
tanerijun <tanerivince@gmail.com>
2026-01-20 09:34:47 +0800

    LC 409

---
+++ leetcode/python/000409.longest-palindrome.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+
+class Solution:
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def longestPalindrome(self, s: str) -> int:
+        counter = Counter(s)
+        res = 0
+        added_orphan = False  # each palindrome can only has at most 1 orphan
+
+        for v in counter.values():
+            if v % 2 == 0:
+                res += v
+            else:
+                res += v - 1  # odd count, add the evens
+                if not added_orphan:
+                    res += 1
+                    added_orphan = True
+
+        return res
