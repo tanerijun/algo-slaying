1458ae09a4e1d0777a87f9d35330d9a48659ad1c
tanerijun <tanerivince@gmail.com>
2026-01-22 08:43:02 +0800

    LC 1436

--- leetcode/python/001436.destination-city.py
+++ leetcode/python/001436.destination-city.py
@@ -15,3 +15,15 @@ class Solution:
             if dst not in graph:
                 return dst
         return ""  # impossible
+
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def destCity1(self, paths: list[list[str]]) -> str:
+        has_path = set()
+        for path in paths:
+            has_path.add(path[0])
+        for path in paths:
+            for city in path:
+                if city not in has_path:
+                    return city
+        return ""  # impossible
