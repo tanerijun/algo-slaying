236b6a0a68ac360902099949c086ee6495ad45f5
tanerijun <tanerivince@gmail.com>
2026-01-27 10:14:27 +0800

    LC 1624

--- leetcode/python/001624.largest-substring-between-two-equal-characters.py
+++ leetcode/python/001624.largest-substring-between-two-equal-characters.py
@@ -17,3 +17,17 @@ class Solution:
                 res = max(res, r - l - 1)
 
         return res
+
+    # Time complexity: O(n)
+    # Space complexity: O(1) -> max 26
+    def maxLengthBetweenEqualCharacters2(self, s: str) -> int:
+        last_index_map = {}
+        for i in range(len(s)):
+            last_index_map[s[i]] = i
+
+        res = -1
+        for i in range(len(s)):
+            if s[i] in last_index_map and last_index_map[s[i]] != i:
+                res = max(res, last_index_map[s[i]] - i - 1)
+
+        return res
