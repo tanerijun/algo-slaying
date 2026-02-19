7fe9fe2fc8e9f5b052d289a708428f8207e37573
tanerijun <tanerivince@gmail.com>
2026-02-19 14:14:54 +0800

    LC 300

--- leetcode/typescript/000300.longest-increasing-subsequence.ts
+++ leetcode/typescript/000300.longest-increasing-subsequence.ts
@@ -14,3 +14,48 @@ function lengthOfLIS(nums: number[]): number {
 
   return Math.max(...dp);
 }
+
+// Time complexity: O(n^2)
+// Space complexity: O(n)
+function LengthOfLIS2(nums: number[]): number {
+  const dp: number[] = Array(nums.length).fill(1);
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  return Math.max(...dp);
+}
+
+// Algorithm:
+// - Greedily maintain a tails array where tails[i] is the smallest possible tail element for any increasing subsequence of length i+1.
+// - For each number:
+//   - If it's larger than everything in tails → extend the LIS by appending it
+//   - Otherwise → replace the first element in tails that is ≥ the current number
+// Time complexity: O(n(log(n)))
+// Space complexity: O(n)
+function LengthOfLIS3(nums: number[]): number {
+  const tails: number[] = [];
+
+  for (const num of nums) {
+    let l = 0;
+    let r = tails.length - 1;
+
+    while (l <= r) {
+      const m = Math.floor((l + r) / 2);
+      if (tails[m] < num) {
+        l = m + 1;
+      } else {
+        r = m - 1;
+      }
+    }
+
+    tails[l] = num;
+  }
+
+  return tails.length;
+}
