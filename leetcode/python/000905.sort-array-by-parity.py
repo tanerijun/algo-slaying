d5a15897de58ed65e15803a9eeff21c83b0c6bfd
tanerijun <tanerivince@gmail.com>
2026-03-15 21:28:26 +0800

    LC 905

---
+++ leetcode/python/000905.sort-array-by-parity.py
@@ -0,0 +1,26 @@
+class Solution:
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def sortArrayByParity(self, nums: list[int]) -> list[int]:
+        res = [0] * len(nums)
+        i, j = 0, len(res) - 1
+        for n in nums:
+            if n % 2 == 0:
+                res[i] = n
+                i += 1
+            else:
+                res[j] = n
+                j -= 1
+        return res
+
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    def sortArrayByParity2(self, nums: list[int]) -> list[int]:
+        i, j = 0, len(nums) - 1
+        while i < j:
+            if nums[i] % 2 != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j -= 1
+            else:
+                i += 1
+        return nums
