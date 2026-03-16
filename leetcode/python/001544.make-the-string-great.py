0f27d23ea48879a242d39600bd215301fd029af9
tanerijun <tanerivince@gmail.com>
2026-03-16 13:11:19 +0800

    LC 1544

--- leetcode/python/001544.make-the-string-great.py
+++ leetcode/python/001544.make-the-string-great.py
@@ -16,3 +16,27 @@ class Solution:
             else:
                 stack.append(ch)
         return "".join(stack)
+
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def makeGood1(self, s: str) -> str:
+        stack: list[str] = []
+        for ch in s:
+            if stack and ch != stack[-1] and ch.lower() == stack[-1].lower():
+                stack.pop()
+            else:
+                stack.append(ch)
+        return "".join(stack)
+
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def makeGood2(self, s: str) -> str:
+        stack: list[str] = []
+        for ch in s:
+            # The difference between a lowercase letter and its uppercase counterpart is exactly 32
+            # (e.g., 'a' is 97 and 'A' is 65)
+            if stack and abs(ord(ch) - ord(stack[-1])) == 32:
+                stack.pop()
+            else:
+                stack.append(ch)
+        return "".join(stack)
